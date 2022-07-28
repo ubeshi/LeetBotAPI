0ae7296153b5b9eeb04f22a9deda77463c2b260c
ColinT <tongcolin@live.ca>
2022-07-27 23:11:16 -0400

    Migrate questions from MongoDB to local json (#1)

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
 const cors = require('cors');
 const express = require('express');
-const mongoose = require('mongoose');
 const questionData = require('./model/question.model');
 
 require('dotenv').config();
 
-const { URI, PORT } = process.env;
+const { PORT } = process.env;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect(URI);
 
 app.get('/', async (req, res) => {
   await questionData.find()
